d212554c4ae78a17003b81ac50f4dc46222496c2
Valandi <valandi.allulis@gmail.com>
2024-10-29 12:46:53 -0400

    interview questions

---
+++ tests/interview-test.spec.js
@@ -0,0 +1,23 @@
+import { expect, test } from '@playwright/test';
+
+// Part 1
+// Write a test that satisfies the following workflow:
+//    1. Navigate to https://sandbox.applitools.com/e-commerce
+//    2. Open the page for "Small Succulent Planter Pot"
+//    3. Choose "gold" for "Select Color" Dropdown
+//    4. Increment count to 3. 
+//    5. Click "Add to cart"
+//    6. Navigate to cart (cart icon in the top right corner)
+//    7. Click "Checkout"
+//    8. Write an assertion to ensure that the total price of the cart is $9.69. 
+
+test('interview test', async ({ page }) => {
+});
+
+// Part 2
+// Write a test that satisfies the following workflow:
+// 1. Navigate to https://the-internet.herokuapp.com/hovers
+// 2. Hover over the first image
+// 3. Write an assertion to ensure that the text "name: user1" is visible
+test('the internet hovers', async ({ page }) => {
+});
